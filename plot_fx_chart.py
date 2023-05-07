0054e40faaf9e7e6c3fed8fbf5047cefbf540300
pingu342 <pingu342@users.noreply.github.com>
2023-05-07 17:51:05 +0900

    mplfinance

--- plot_fx_chart.py
+++ plot_fx_chart.py
@@ -6,9 +6,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.finance as mf
+import mplfinance as mf
 from matplotlib.dates import date2num
-
+from mplfinance.original_flavor import candlestick_ochl
 
 # In[35]:
 
@@ -42,7 +42,7 @@ ax = fig.add_subplot(111)
 fig.subplots_adjust(top=0.85)
 ax.grid()
 ax.set_xlim(date[0], date[-1])
-mf.candlestick_ochl(ax, quote, width=0.02, colorup='g', colordown='r', alpha=0.75)
+candlestick_ochl(ax, quote, width=0.02, colorup='g', colordown='r', alpha=0.75)
 fig.autofmt_xdate()
 plt.show()
 
